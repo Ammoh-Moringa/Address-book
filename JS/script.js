032831155c4f0e192a2471dfa6724816a070724f
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-04-21 23:44:42 +0300

    Add js function

--- JS/script.js
+++ JS/script.js
@@ -56,9 +56,9 @@ $(document).ready(function () {
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    $("input#new-street").val("");
-    $("input#new-city").val("");
-    $("input#new-county").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-county").val("");
 
   });
 
